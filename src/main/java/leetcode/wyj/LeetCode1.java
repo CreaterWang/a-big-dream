c485fa415bb7d538b10b92dce3d2580cc25c6fe8
贾玄玄 <496272885@qq.com>
2020-07-05 21:19:49 +0800

    两数相加

---
+++ src/main/java/leetcode/wyj/LeetCode1.java
@@ -0,0 +1,52 @@
+package leetcode.wyj;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] array = null;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int findNum = target - nums[i];
+            Integer index = map.get(findNum);
+            if (index != null && index != i) {
+                return new int[]{i, index};
+            }
+        }
+        return null;
+    }
+
+}
