ee1cc854a55aee20bcaf0c7e83a4cd858b19be53
贾玄玄 <496272885@qq.com>
2020-07-08 23:04:37 +0800

    虚拟机笔记

---
+++ src/main/java/leetcode/wyj/Demo.java
@@ -0,0 +1,15 @@
+package leetcode.wyj;
+
+public class Demo {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        int a = 1;
+        int b = 2;
+        int sum = demo.add(a, b);
+        System.out.println(sum);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
