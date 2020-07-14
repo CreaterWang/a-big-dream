e899eee74350f0ee5a94b64ce092cbe0aa34e597
贾玄玄 <496272885@qq.com>
2020-07-14 22:43:12 +0800

    leetcode

--- src/main/java/leetcode/wyj/LeetCode17.java
+++ src/main/java/leetcode/wyj/LeetCode17.java
@@ -2,6 +2,22 @@ package leetcode.wyj;
 
 import java.util.*;
 
+/**
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * <p>
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * <p>
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LeetCode17 {
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
@@ -30,6 +46,7 @@ public class LeetCode17 {
         return list;
 
     }
+
     private List<String> combine(List<String> str1, List<String> str2) {
         List<String> result = new ArrayList<>();
         for (int i = 0; i < str1.size(); i++) {
