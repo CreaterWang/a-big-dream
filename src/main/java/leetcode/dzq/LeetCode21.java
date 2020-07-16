9c5a12dc3c05f5945dee507feca1cdf21315749a
dongzeqi <2216620840@qq.com>
2020-07-16 20:31:55 +0800

    算法21

--- src/main/java/leetcode/dzq/LeetCode21.java
+++ src/main/java/leetcode/dzq/LeetCode21.java
@@ -25,9 +25,30 @@ import leetcode.model.ListNode;
  */
 public class LeetCode21 {
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = null;
+//        l1.next = new ListNode(2);
+//        l1.next.next = new ListNode(4);
+//        ListNode l2 = new ListNode(1);
+//        l2.next = new ListNode(3);
+//        l2.next.next = new ListNode(4);
+        System.out.println(mergeTwoLists(l1,l2));
 
     }
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+        ListNode listNode1 = new ListNode(0);
+        ListNode listNode = listNode1;
+        while (l1 != null || l2 != null ){
+            if (l1 == null || (l1 != null && l2 != null && l1.val > l2.val)){
+                listNode1.next = l2;
+                l2 = l2.next;
+            }else{
+                listNode1.next = l1;
+                l1 = l1.next;
+            }
+            listNode1 = listNode1.next;
+
+        }
+        return listNode.next;
     }
 }
